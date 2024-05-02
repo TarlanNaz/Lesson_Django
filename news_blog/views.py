294b8865b816dc18016457210be267ea9cf7429a
TarlanNaz <tarlnnaz07@gmail.com>
2024-05-02 16:03:08 +0600

    2 zadanie (full)

--- news_blog/views.py
+++ news_blog/views.py
@@ -18,7 +18,13 @@ def index(request):
     return render(request,'news_blog/news_list.html', {'articles': articles})
 
 def get_article(request, article_id,):
-    return render(request, 'news_blog/news_article.html', {'article': articles[article_id - 1]})
+    article=None
+    for i in articles:
+        if i['id'] == article_id:
+            article = i
+            break
+
+    return render(request, 'news_blog/news_article.html', {'article': article})
 
 def me(request):
     return render(request,'news_blog/about.html',)
