ac51dbaf95332e6537456242d12e680992da9d81
TarlanNaz <tarlnnaz07@gmail.com>
2024-05-06 20:49:15 +0600

    problem

--- news_blog/models.py
+++ news_blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+
+class Article(models.Model):
+    title  = models.CharField(max_length=100)
+    created_date = models.DateTimeField(Default = timezone.now)
+    text = models.TextField()
+    published_date = models.DateTimeField(blank = True, null = True)
 
 # Create your models here.
