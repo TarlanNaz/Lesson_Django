cb6ded1367432e07cd017af18df636ba7d426047
TarlanNaz <tarlnnaz07@gmail.com>
2024-05-07 16:04:53 +0600

    3 zadanie

--- news_blog/models.py
+++ news_blog/models.py
@@ -1,10 +1,30 @@
+
+
+import uuid
 from django.db import models
 from django.utils import timezone
 
-class Article(models.Model):
-    title  = models.CharField(max_length=100)
-    created_date = models.DateTimeField(Default = timezone.now)
-    text = models.TextField()
-    published_date = models.DateTimeField(blank = True, null = True)
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
 
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def remove(self):
+        self.delete()
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.created_date)
 # Create your models here.
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,36 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .models import Article, Author
 
 
-articles = [
-   {
-       'id': 1,
-       'title': 'First news',
-       'text': 'This is the worst first article'
-   },
-   {
-       'id': 2,
-       'title': 'Second news',
-       'text': 'This is the amazing second article'
-   }]
+
+
 
 # Create your views here.
 def index(request):
+    articles=Article.objects.all()
     return render(request,'news_blog/news_list.html', {'articles': articles})
 
-def get_article(request, article_id,):
-    article=None
-    for i in articles:
-        if i['id'] == article_id:
-            article = i
-            break
-
-    return render(request, 'news_blog/news_article.html', {'article': article})
+def get_article(request, article_id):
+   try:
+       article = Article.objects.get(id=article_id)
+       author = Author.objects.filter(article__pk=article_id)
+   except (Article.DoesNotExist, Author.DoesNotExist) as error:
+       return HttpResponseNotFound(error)
+   return render(request, 'news_blog/news_article.html',
+                 {'article': article,
+                  'author': author
+                  }
+                 )
 
 def me(request):
     return render(request,'news_blog/about.html',)
 
-#def get_
+
 
 
 
